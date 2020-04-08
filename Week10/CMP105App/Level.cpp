9e86cb332b039fa39f37e75abe579b16970af959
1900608 Elliot Stevenson <1900608@uad.ac.uk>
2020-04-08 18:45:19 +0100

    Update Level.cpp

--- Week10/CMP105App/Level.cpp
+++ Week10/CMP105App/Level.cpp
@@ -7,6 +7,43 @@ Level::Level(sf::RenderWindow* hwnd, Input* in)
 
 	// initialise game objects
 
+	tilemap.loadTexture("gfx/marioTiles.png");
+
+	std::vector<GameObject> tiles;
+	for (int i = 0; i < 7; i++)
+	{
+		tile.setSize(sf::Vector2f(32, 32));
+		tile.setCollisionBox(0, 0, 32, 32);
+		tile.setCollider(true);
+		tiles.push_back(tile);
+	}
+
+	tiles[0].setCollider(false);
+	tiles[0].setTextureRect(sf::IntRect(187, 51, 16, 16));
+	tiles[1].setTextureRect(sf::IntRect(0, 0, 16, 16));
+	tiles[2].setTextureRect(sf::IntRect(17, 0, 16, 16));
+	tiles[3].setTextureRect(sf::IntRect(34, 0, 16, 16));
+	tiles[4].setTextureRect(sf::IntRect(0, 34, 16, 16));
+	tiles[5].setTextureRect(sf::IntRect(17, 34, 16, 16));
+	tiles[6].setTextureRect(sf::IntRect(34, 34, 16, 16));
+
+	tilemap.setTileSet(tiles);
+
+	sf::Vector2u mapSize(10, 6);
+	std::vector<int> map = {
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 1, 3, 0, 0, 0, 0, 0,
+		1, 2, 3, 4, 6, 1, 2, 3, 0, 0,
+		4, 5, 6, 0, 0, 4, 5, 6, 0, 0
+	};
+
+	int posx = window->getSize().x;
+	int posy = window->getSize().y;
+	tilemap.setTileMap(map, mapSize);
+	tilemap.setPosition(sf::Vector2f(posx - posx, posy- posy));
+	tilemap.buildLevel();
 }
 
 Level::~Level()
@@ -30,7 +67,7 @@ void Level::update(float dt)
 void Level::render()
 {
 	beginDraw();
-
+	tilemap.render(window);
 	endDraw();
 }
 
